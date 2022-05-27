2f8300dd89e66953366b9eef141385cdb744ccb1
xwzhou <xwzhou@yeah.net>
2022-05-27 10:46:41 +0800

    fix

--- gen-self-ssl.sh
+++ gen-self-ssl.sh
@@ -2,40 +2,35 @@
 set -e
 
 defaultPass="pass"
-defaultSubject="/C=CN/ST=ST/L=L/O=O/OU=OU"
+defaultSubject="/C=CN/ST=ST/L=L/O=O/OU=OU/CN="
 defaultDays="3650"
 defaultBit="2048"
-defaultExtfileData="extendedKeyUsage = serverAuth\nsubjectAltName = DNS.1:example.com,IP.1:127.0.0.1"
 
 HOST=${HOST}
 PASS=${PASS:-$defaultPass}
 SUBJECT=${SUBJECT}
 DAYS=${DAYS:-$defaultDays}
 BIT=${BIT:-$defaultBit}
-EXTFILE=${EXTFILE}
+SERVER_EXTFILE=${SERVER_EXTFILE}
+IS_SERVER_EXTFILE_CREATED=0
 CA=${CA}
 PKCS12=${PKCS12}
-
-rebuild_subject() {
-    if [ -z "$SUBJECT" ]; then
-        SUBJECT=${SUBJECT:-$defaultSubject}"/CN="$HOST
-    fi
-}
 
 help() {
     echo "Usage: "
     echo "  --host, -h: hostname, domain name or ip, required."
     echo "  --ca: create ca cert."
-    echo "  --pass, -p: the password of cert, default '$defaultPass'."
-    echo "  --days, -d: days, default '$defaultDays'."
-    echo "  --subj: subject, default '$defaultSubject/CN=\{host}'."
+    echo "  --pass, -p: the password of cert, default $defaultPass."
+    echo "  --days, -d: days, default $defaultDays."
+    echo "  --subj: subject, default /C=CN/ST=ST/L=L/O=O/OU=OU/CN={host}."
     echo "  --bit: numbits, default '$defaultBit'."
-    echo "  --exfile: exfile, default '', example --exfile=exfile.cnf."
+    echo "  --server-exfile: default '', example --server-exfile=server-exfile.cnf."
     echo "  --help: help"
     echo "Example:"
     echo "  gen-self-ssl.sh --host=localhost --days=3650"
     echo "Note exfile.cnf data:"
-    echo -e "$defaultExtfileData"
+    echo "  extendedKeyUsage = serverAuth"
+    echo "  subjectAltName = DNS.1:example.com,IP.1:127.0.0.1"
 }
 
 log() {
@@ -78,11 +73,6 @@ check_file() {
     fi
 }
 
-clean() {
-    rm -f server-csr.pem client-csr.pem ca-key.pem ca.srl
-    rm -f ca.pem client-cert.pem client-key.pem ca.pem server-cert.pem server-key.pem server.p12 client.p12
-}
-
 parse_flags() {
     k=""
     v=""
@@ -96,7 +86,7 @@ parse_flags() {
         "--subj" | "-subj") SUBJECT=$v ;;
         "--pass" | "-pass" | "-p") PASS=$v ;;
         "--days" | "-days" | "-d") DAYS=$v ;;
-        "--extfile" | "-extfile") EXTFILE=$v ;;
+        "--server-extfile") SERVER_EXTFILE=$v ;;
         "--clean" | "-clean")
             clean
             exit
@@ -112,10 +102,44 @@ parse_flags() {
             ;;
         esac
     done
-    rebuild_subject
 }
 
-build() {
+initial() {
+
+    parse_flags $*
+
+    if [ -z "$HOST" ]; then
+        help
+        exit
+    fi
+
+    if ! command_exists openssl; then
+        echo "OpenSSL isn't installed. You need that to generate SSL certificates."
+        exit
+    fi
+
+    if [ -z "$SUBJECT" ]; then
+        SUBJECT=${SUBJECT:-"/C=CN/ST=ST/L=L/O=O/OU=OU/CN=$HOST"}
+    fi
+
+    if [ -z "$SERVER_EXTFILE" ]; then
+        SERVER_EXTFILE="server-extfile.cnf"
+        if [ ! -f "$SERVER_EXTFILE" ]; then
+            echo -e "extendedKeyUsage = serverAuth\nsubjectAltName = DNS.1:$HOST,IP.1:127.0.0.1" >"$SERVER_EXTFILE"
+            IS_SERVER_EXTFILE_CREATED=1
+            log_warn "create template file $SERVER_EXTFILE."
+        fi
+    fi
+
+    log "..."
+    cat "$SERVER_EXTFILE"
+    log "..."
+
+    rm -f server-csr.pem client-csr.pem ca-key.pem ca.srl
+    rm -f ca.pem client-cert.pem client-key.pem ca.pem server-cert.pem server-key.pem server.p12 client.p12
+}
+
+service() {
     if [ -n "$CA" ]; then
         # build ca
         command_openssl genrsa -passout pass:$PASS -out ca-key.pem $BIT
@@ -136,25 +160,12 @@ build() {
     check_file server-csr.pem
     log "server-csr.pem created."
 
-    v_path_extfile="$EXTFILE"
-    v_autoremove=0
-    if [ -z $v_path_extfile ]; then
-        v_autoremove=1
-        v_path_extfile="server-extfile.cnf"
-        echo -e "extendedKeyUsage = serverAuth\nsubjectAltName = DNS.1:$HOST,IP.1:127.0.0.1" >"$v_path_extfile"
-        log_warn "create template file $v_path_extfile."
-    fi
-
-    log "..."
-    cat "$v_path_extfile"
-    log "..."
-
     CMD_PARAMS=" -signkey server-key.pem"
     if [ -n "$CA" ]; then
         CMD_PARAMS=" -CAcreateserial -CA ca.pem -CAkey ca-key.pem"
     fi
 
-    command_openssl x509 -req -days $DAYS -in server-csr.pem $CMD_PARAMS -out server-cert.pem -extfile "$v_path_extfile"
+    command_openssl x509 -req -days $DAYS -in server-csr.pem $CMD_PARAMS -out server-cert.pem -extfile "$SERVER_EXTFILE"
     check_file server-cert.pem
     log "server-cert.pem created."
 
@@ -162,11 +173,6 @@ build() {
         command_openssl pkcs12 -export -in server-cert.pem -inkey server-key.pem -out server.p12 -passin pass:$PASS -passout pass:$PASS
         check_file server.p12
         log "server.p12 created."
-    fi
-
-    if [ $v_autoremove = 1 ]; then
-        log_warn "remove template file $v_path_extfile."
-        rm -f "$v_path_extfile"
     fi
 
     # build client cert
@@ -192,7 +198,14 @@ build() {
         log "client.p12 created."
     fi
 
+}
+
+finish() {
     rm -f server-csr.pem client-csr.pem ca-key.pem ca.srl
+    if [ $IS_SERVER_EXTFILE_CREATED = 1 ]; then
+        log_warn "remove template file $SERVER_EXTFILE."
+        rm -f "$SERVER_EXTFILE"
+    fi
 
     log ""
     log "Output:"
@@ -215,26 +228,14 @@ build() {
     fi
     log "client side: client-cert.pem client-key.pem $CMD_PARAMS"
     log_suc "---------Sucessfuly----------"
-
 }
 
 main() {
-    parse_flags $*
+    initial $*
 
-    if [ -z "$HOST" ]; then
-        help
-        exit
-    fi
+    service
 
-    if ! command_exists openssl; then
-        echo "OpenSSL isn't installed. You need that to generate SSL certificates."
-        exit
-    fi
-
-    clean
-
-    build
-
+    finish
 }
 
 main $*
